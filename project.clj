ad684b4ff203611c18acd7449dbe9624a12499b7
adutton <alistair.dutton@nokia.com>
2013-09-06 17:08:21 +0100

    Modified to use clansi directly.

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject colourizer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [
+                 [org.clojure/clojure "1.5.1"]
+                 [clansi "1.0.0"]]
   :main colourizer.core
   :profiles {:uberjar {:aot :all}})
